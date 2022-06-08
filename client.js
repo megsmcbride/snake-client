c0d864eafb0b10034f1f3c2756554f86cf85301f
megan <meganambermcbride@gmail.com>
2022-06-08 15:58:26 -0400

    client exports connection while play requires client.

---
+++ client.js
@@ -0,0 +1,25 @@
+const { builtinModules } = require("module");
+const net = require("net");
+
+const connect = (() => {
+  const conn = net.createConnection({
+    host: '10.0.0.57',
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("You are connected");
+  });
+  conn.on("data", (data) => {
+    console.log(`${data}`);
+  });
+
+  return conn;
+
+});
+
+console.log("Connecting...");
+connect();
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const net = require("net");
+const { connect } = require("./client")
 
-const connect = (() => {
-  const conn = net.createConnection({
-    host: '10.0.0.57',
-    port: 50541,
-  });
-
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("You are connected");
-  });
-  conn.on("data", (data) => {
-    console.log(`${data}`);
-  });
-
-  return conn;
-
-});
-
-console.log("Connecting...");
-connect();
+connect;
