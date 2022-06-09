ce474436e8acf2a0471231859c88c51ff2c393cc
megan <meganambermcbride@gmail.com>
2022-06-08 20:10:29 -0400

    refactored

--- input.js
+++ input.js
@@ -17,17 +17,13 @@ const setupInput = (conn) => {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
-  }
-  if (key === "w") {
+  } else if (key === "w") {
     connection.write("Move: up");
-  } 
-  if (key === "a") {
+  } else if (key === "a") {
     connection.write("Move: left");
-  }
-  if (key === "d") {
+  } else if (key === "d") {
     connection.write("Move: right");
-  }
-  if (key === "s") {
+  } else if (key === "s") {
     connection.write("Move: down");
   }
 };
